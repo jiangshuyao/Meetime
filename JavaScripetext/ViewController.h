6dd75e4ec3b476351fec103730a6bf17798958a5
fengrongheng <fengrongheng@rrslj.com>
2016-04-20 13:09:52 +0800

    Initial Commit

---
+++ JavaScripetext/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JavaScripetext
+//
+//  Created by Rongheng on 16/4/20.
+//  Copyright © 2016年 Rongheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JavaScripetext/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JavaScripetext
+//
+//  Created by Rongheng on 16/4/20.
+//  Copyright © 2016年 Rongheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
