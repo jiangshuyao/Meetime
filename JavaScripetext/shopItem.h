ec627b8b1acd388f3922ccc27bcaef5373a04c54
舒耀哥 <shuyao5261@163.com>
2016-09-02 16:19:13 +0800

    添加shopItem文件

---
+++ JavaScripetext/shopItem.h
@@ -0,0 +1,13 @@
+//
+//  shopItem.h
+//  JavaScripetext
+//
+//  Created by Admin on 16/9/2.
+//  Copyright © 2016年 Rongheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface shopItem : NSObject
+
+@end
---
+++ JavaScripetext/shopItem.m
@@ -0,0 +1,13 @@
+//
+//  shopItem.m
+//  JavaScripetext
+//
+//  Created by Admin on 16/9/2.
+//  Copyright © 2016年 Rongheng. All rights reserved.
+//
+
+#import "shopItem.h"
+
+@implementation shopItem
+
+@end
